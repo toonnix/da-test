9ea590ca9226a3483b5610a15b42752eaf57efc2
Toon <sagsun.d@gmail.com>
2021-03-07 20:47:18 +0700

    again

--- src/HeroPage.tsx
+++ src/HeroPage.tsx
@@ -16,24 +16,24 @@ export interface HeroInfo {
 }
 
 export const HeroPage: React.FC = () => {
-    let { heroId } = useParams<HeroParams>();
+    const { heroId } = useParams<HeroParams>();
 
     const [heroInfo, setHeroInfo] = useState<HeroInfo>({
         name: '',
         description: '',
     });
-    const privateKey = 'e94ca8202f710e551f0065740b10772dccee1848';
-    const publicKey = '3c36ca4b2e113ad8aabc725a3cca1bf0';
-    const ts = moment().unix();
-    const hashKey = md5(`${ts}${privateKey}${publicKey}`);
-    const callUrl = `https://gateway.marvel.com/v1/public/characters/${heroId}?ts=${ts}&apikey=${publicKey}&hash=${hashKey}&nameStartsWith=spider`;
 
     useEffect(() => {
         let mounted = true;
+        const _heroId = heroId;
+        const privateKey = 'e94ca8202f710e551f0065740b10772dccee1848';
+        const publicKey = '3c36ca4b2e113ad8aabc725a3cca1bf0';
+        const ts = moment().unix();
+        const hashKey = md5(`${ts}${privateKey}${publicKey}`);
+        const callUrl = `https://gateway.marvel.com/v1/public/characters/${_heroId}?ts=${ts}&apikey=${publicKey}&hash=${hashKey}&nameStartsWith=spider`;
 
         axios.get(callUrl).then((res) => {
             const results = res.data.data.results[0];
-            console.log(results);
             if (mounted) {
                 setHeroInfo(results)
             }
